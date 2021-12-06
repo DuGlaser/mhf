bb66056faa9b363f3023972d930dab74b3e1f203
DuGlaser <duglaser0618@gmail.com>
2021-12-06 15:39:34 +0900

    :sparkles: Add the Router model.

--- mhf/mhf.go
+++ mhf/mhf.go
@@ -9,6 +9,10 @@ import (
 
 type Mhf struct {
 	server *http.Server
+	router *Router
+}
+
+type Router struct {
 	routes map[string]*Route
 }
 
@@ -24,7 +28,9 @@ type MiddlewareFunc func(http.HandlerFunc) http.HandlerFunc
 func New() *Mhf {
 	m := &Mhf{
 		server: new(http.Server),
-		routes: make(map[string]*Route),
+		router: &Router{
+			routes: make(map[string]*Route),
+		},
 	}
 
 	m.server.Handler = m
@@ -35,7 +41,7 @@ func (m *Mhf) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	method := r.Method
 	path := r.URL.Path
 
-	route, err := m.find(method, path)
+	route, err := m.router.find(method, path)
 	if err != nil {
 		w.WriteHeader(404)
 		return
@@ -62,26 +68,12 @@ func (m *Mhf) add(method, path string, handler http.HandlerFunc, middlewares ...
 		path = fmt.Sprintf("/%s", path)
 	}
 
-	if m.routes[method+path] != nil {
+	if r, _ := m.router.find(method, path); r != nil {
 		fmt.Printf("%s(%s) is already resisted.", path, method)
 		return
 	}
 
-	m.routes[method+path] = &Route{
-		method:      method,
-		path:        path,
-		handler:     handler,
-		middlewares: middlewares,
-	}
-}
-
-func (m *Mhf) find(method, path string) (*Route, error) {
-	r := m.routes[method+path]
-	if r == nil {
-		return nil, errors.New(fmt.Sprintf("%s(%s) is not found", path, method))
-	}
-
-	return r, nil
+	m.router.add(method, path, handler, middlewares...)
 }
 
 func (m *Mhf) Get(path string, handler http.HandlerFunc, middlewares ...MiddlewareFunc) {
@@ -98,4 +90,22 @@ func (m *Mhf) Put(path string, handler http.HandlerFunc, middlewares ...Middlewa
 
 func (m *Mhf) Delete(path string, handler http.HandlerFunc, middlewares ...MiddlewareFunc) {
 	m.add("DELETE", path, handler, middlewares...)
+}
+
+func (r *Router) add(method, path string, handler http.HandlerFunc, middlewares ...MiddlewareFunc) {
+	r.routes[method+path] = &Route{
+		method:      method,
+		path:        path,
+		handler:     handler,
+		middlewares: middlewares,
+	}
+}
+
+func (r *Router) find(method, path string) (*Route, error) {
+	rs := r.routes[method+path]
+	if rs == nil {
+		return nil, errors.New(fmt.Sprintf("%s(%s) is not found", path, method))
+	}
+
+	return rs, nil
 }
